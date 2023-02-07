e6af34ac24599adbac9d8fa7b67ea0776d57c51c
Jesús Paredes <jjesus.paredesl@gmail.com>
2023-02-07 07:15:34 -0300

    pre-desafio-1

--- main.js
+++ main.js
@@ -2,37 +2,90 @@
 
 class ProductManager {
 
-  constructor (title, description, price, thumbnail, code, stock){
-
+  constructor (){
       this.products = []
-      this.title = title
-      this.description =  description
-      this.price =  price
-      this.thumbnail =  thumbnail
-      this.code = code
-      this.stock = stock
-
     }
 
-addProduct (){
+static id = 0
 
-    this.products.push([{  
-         id: this.products.length +1
-        }])  
+  addProduct (title, description, price, thumbnail, code, stock){
+    
+    for (let i = 0; i < this.products.length; i++){
 
+      if(this.products[i].code===code){
+        console.log(`The code ${code} is repetead`)
+        break
       }
+    }
 
-getProducts (){
+    const newProduct = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+    }
+    
+    if(!Object.values(newProduct).includes(undefined)){
+
+      ProductManager.id++;
+      this.products.push( {
+
+       ...newProduct,
+        id:ProductManager.id
+      })
+    } else {
+      console.log('All fields are required')
+    }
+   
+  }
+
+  getProducts (){
   
     return this.products
+  }
+
+exist(id){
+  return this.products.find((productElement)=> productElement.id === id)
 }
 
+  getProductById (id){
 
-
-getProductById (){
-  
-this.products.find(e => e.id === idproduct) 
+    if(!this.exist(id)){
+      console.log('Not Found')
+    }
+    else {
+      console.log(this.exist(id))
+    }
+    
+  }
 
 }
 
-}
+const productos = new ProductManager
+
+//Llamada con arreglo vacio
+
+console.log(productos.getProducts())
+
+//Agregando Productos
+
+productos.addProduct('titulo1', 'description1', '402' , 'imagen1' , 'code123' , 5)
+productos.addProduct('titulo2', 'description2', '402' , 'imagen2' , 'code1234' , )
+
+//Llamada con arreglo de producto
+console.log(productos.getProducts())
+
+
+productos.addProduct('titulo3', 'description3', '402' , 'imagen3' , 'code1234' , 7)
+
+
+//Busqueda producto por ID
+
+productos.getProductById(2)
+
+
+//Busqueda producto por ID no encontrado
+
+productos.getProductById(5)
